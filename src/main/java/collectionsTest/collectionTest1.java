bd68043804652f4d2fdf94a6514ddd21f8de7575
madis12345 <40760868+madis12345@users.noreply.github.com>
2018-07-02 20:56:56 +0530

    commiting print chnages

--- src/main/java/collectionsTest/collectionTest1.java
+++ src/main/java/collectionsTest/collectionTest1.java
@@ -2,7 +2,7 @@ package collectionsTest;
 
 public class collectionTest1 {
 	public static void main(String[] args) {
-		System.out.println("Hello collections");
+		System.out.println("Hello collections 123");
 		
 	}
 }
